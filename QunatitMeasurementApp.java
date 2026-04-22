85d07cff2a15af3018eb3c0e414cf4d79afe5b81
Dinesh Beniwal <dineshbeniwal1515@gmail.com>
2026-04-22 09:42:46 +0530

    UC4: Add Yards and Centimeters support using enum scalability

--- QunatitMeasurementApp.java
+++ QunatitMeasurementApp.java
@@ -1,9 +1,12 @@
 public class QuantityMeasurementApp {
 
-    // ENUM for units (base = feet)
+    // ENUM (base unit = feet)
     enum LengthUnit {
+
         FEET(1.0),
-        INCH(1.0 / 12);
+        INCH(1.0 / 12),
+        YARD(3.0),                 // 1 yard = 3 feet
+        CENTIMETER(0.0328084);     // 1 cm ≈ 0.0328084 feet
 
         private final double toFeet;
 
@@ -49,9 +52,14 @@ public class QuantityMeasurementApp {
     // Demo
     public static void main(String[] args) {
 
-        Quantity q1 = new Quantity(1.0, LengthUnit.FEET);
-        Quantity q2 = new Quantity(12.0, LengthUnit.INCH);
+        Quantity q1 = new Quantity(1.0, LengthUnit.YARD);
+        Quantity q2 = new Quantity(3.0, LengthUnit.FEET);
 
-        System.out.println("Equal? " + q1.equals(q2)); // true
+        System.out.println("Yard vs Feet: " + q1.equals(q2)); // true
+
+        Quantity q3 = new Quantity(1.0, LengthUnit.CENTIMETER);
+        Quantity q4 = new Quantity(0.393701, LengthUnit.INCH);
+
+        System.out.println("CM vs Inch: " + q3.equals(q4)); // true
     }
 }
